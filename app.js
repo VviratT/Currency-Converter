39ddc17c7cdd70f081cbbe97c91bf070915d83c3
VviratT <123719148+VviratT@users.noreply.github.com>
2024-08-17 14:33:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest/";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const amount = document.querySelector(".amount input");
+
+
+for (let select of dropdowns) {
+  for (const currCode in countryList) {
+    const newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  const amtVal = amount.value.trim();
+  if (amtVal === "" || isNaN(amtVal) || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}${fromCurr.value}`;
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const rate = data.rates[toCurr.value];
+    const finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    console.error(error);
+    msg.innerText = "Error: Unable to retrieve exchange rate";
+  }
+};
+
+const updateFlag = (element) => {
+  const currCode = element.value;
+  const countryCode = countryList[currCode];
+  const newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  const img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
